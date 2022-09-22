394cca0aac47c6046f68bd2f0ebec3cbe1ed6fbc
charlie retzler <charlie.retzler@gmail.com>
2022-09-22 09:03:04 -0400

    Skylar and Charlie's implementation of bit compare and sort

--- Templates/Java/src/main/java/Kata.java
+++ Templates/Java/src/main/java/Kata.java
@@ -1,5 +1,66 @@
+import java.util.*;
+
 public class Kata {
-    public String hello(String name) {
-        return String.format("Hello %s", name);
+    public int[] bitSort(int[] numbers) {
+        int[] sortedNums = new int[numbers.length];
+
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>() {};
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            int number = numbers[i];
+            int bitsCount = bitCounter(number);
+
+            if (!map.containsKey(bitsCount)){
+                map.put(bitsCount, new ArrayList<>());
+            }
+
+            ArrayList<Integer> item = map.getOrDefault(bitsCount, new ArrayList<>());
+            item.add(number);
+        }
+
+        ArrayList<Integer> results = new ArrayList<>();
+        for (int bitSet = 0; bitSet < 32; bitSet++) {
+            if (map.containsKey(bitSet)) {
+                ArrayList<Integer> originalNumbers = map.get(bitSet);
+                Collections.sort(originalNumbers);
+                System.out.println(originalNumbers);
+                results.addAll(originalNumbers);
+            }
+
+        }
+        return listToArray(results);
     }
+
+    public int[] listToArray(ArrayList<Integer> input) {
+        int[] results = new int[input.toArray().length];
+        for (int index = 0; index < input.toArray().length; index++) {
+            results[index] = input.get(index);
+        }
+        return results;
+    }
+
+    public int bitCounter(int value) {
+        int bitCounter = 0;
+        String numToBits = Integer.toBinaryString(value);
+
+        for (int index = 0; index < numToBits.length(); index++) {
+            if (numToBits.charAt(index) == '1') {
+                bitCounter++;
+            }
+        }
+        return bitCounter;
+    }
+
+
+//
+//    Initialize count: = 0
+//    If integer n is not zero
+//            (a) Do bitwise & with (n-1)
+//    and assign the value
+//    back to n
+//    n: = n&(n-1)
+//            (b) Increment count by 1
+//            (c) go to step 2
+//    Else return count
 }
--- Templates/Java/src/test/java/KataTest.java
+++ Templates/Java/src/test/java/KataTest.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Unsigned;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +13,57 @@ public class KataTest {
     }
 
     @Test
-    public void helloShouldReturnMessageWithSpecifiedName() {
-        String name = "Ingage";
-        assertEquals(String.format("Hello %s", name), fixture.hello(name));
+    public void givenAnArrayWithOneElement_return_one_element_sorted() {
+        int[] values = new int []{4};
+        int[] actual = fixture.bitSort(values);
+        assertEquals(4, actual[0]);
     }
+
+    @Test
+    public void givenAnArrayWithTwoElement_return_sorted() {
+        int[] values = new int []{7, 6};
+        int[] actual = fixture.bitSort(values);
+        assertEquals(2, actual.length);
+        assertEquals(6, actual[0]);
+        assertEquals(7, actual[1]);
+    }
+
+    @Test
+    public void givenAnArrayWithTies_return_sorted_by_bits_then_by_number() {
+        int[] values = new int []{4, 2, 1};
+        int[] actual = fixture.bitSort(values);
+        assertEquals(3, actual.length);
+        assertEquals(1, actual[0]);
+        assertEquals(2, actual[1]);
+        assertEquals(4, actual[2]);
+    }
+
+    @Test
+    public void given_7_return_3() {
+        int value = 7;
+        int actual = fixture.bitCounter(value);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    public void given_9_return_2() {
+        int value = 9;
+        int actual = fixture.bitCounter(value);
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void given_31_return_5() {
+        int value = 31;
+        int actual = fixture.bitCounter(value);
+        assertEquals(5, actual);
+    }
+
+    @Test
+    public void given_max_int_return_31() {
+        int value = Integer.MAX_VALUE;
+        int actual = fixture.bitCounter(value);
+        assertEquals(31, actual);
+    }
+
 }
